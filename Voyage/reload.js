a44c010181840aacba716e0f3555f23fcf374e0c
Cyprien-png <61788240+Cyprien-png@users.noreply.github.com>
2020-06-04 14:01:02 +0200

    Re organisation

---
+++ Voyage/reload.js
@@ -0,0 +1,7 @@
+txtTitle09.value = 'Titre'
+
+    // Load image tags with the name in the input field
+    document.querySelectorAll(".actimg").forEach(img => {
+        img.src = document.getElementById('txt'+img.id.substring(3)).value
+    });
+    
